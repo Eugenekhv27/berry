4127546fe9164b50cef8c7d88f1c9ca6a496ecb9
snizh <snizh@list.ru>
2017-09-02 14:21:44 +1000

    Поместил маршруты, разрешенные только для залогиненного пользователя в одну группу (с общим вызовом проверки).

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './services/auth-guard.service';
 import { AdminComponent} from './admin.component';
 import { AdminLoginComponent} from './login/login.component';
 import { AdminHelpComponent } from './help/help.component';
@@ -11,19 +10,26 @@ import { AdminCircularComponent } from './circular/circular.component';
 import { AdminReferralSettingsComponent } from './settings/referral-settings.component';
 import { AdminDocumentationComponent } from './documentation/documentation.component';
 
+import { AuthGuard } from './services/auth-guard.service';
+
 const routes: Routes = [{
     path: '',
     component: AdminComponent,
     children: [
-      { path: 'dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard]  },
-      { path: 'participants-list', component: AdminParticipantsListComponent, canActivate: [AuthGuard]  },
-      { path: 'circular', component: AdminCircularComponent, canActivate: [AuthGuard]  },
-      { path: 'bonus-accounting', component: AdminParticipantsListComponent, canActivate: [AuthGuard]  },
-      { path: 'settings', component: AdminReferralSettingsComponent, canActivate: [AuthGuard]  },
       { path: 'documentation', component: AdminDocumentationComponent },
       { path: 'login', component: AdminLoginComponent },
-      { path: 'help', component: AdminHelpComponent },
-      { path: '**', component: AdminLoginComponent }
+      { path: '',
+        canActivate: [AuthGuard],
+        children: [
+          { path: 'dashboard', component: AdminDashboardComponent  },
+          { path: 'participants-list', component: AdminParticipantsListComponent },
+          { path: 'circular', component: AdminCircularComponent },
+          { path: 'bonus-accounting', component: AdminParticipantsListComponent },
+          { path: 'settings', component: AdminReferralSettingsComponent },
+          { path: 'help', component: AdminHelpComponent },
+        ]
+      }
+//      { path: '**', component: AdminLoginComponent }
     ]
 }];
 
