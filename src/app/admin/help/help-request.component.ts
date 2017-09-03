1867b5d4c4222a45adfe8c0e8d9934f7891e9cb2
snizh <snizh@list.ru>
2017-09-03 23:18:00 +1000

    Удалил из компонента help-request ненужные строки и ненужные зависимости.

--- src/app/admin/help/help-request.component.ts
+++ src/app/admin/help/help-request.component.ts
@@ -1,47 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-import {
-  InputTextModule, MegaMenuModule,
-  MenuItem, OverlayPanelModule,
-  OverlayPanel
-} from 'primeng/primeng';
 import { DataService } from '../../mocks/services/data.service';
-
-
 import { NotifierService } from '../notifier/notifier.service';
 
 @Component({
   selector: 'app-help-request',
-  templateUrl: './help-request.component.html',
-  providers: [DataService]
+  templateUrl: './help-request.component.html'
 })
 export class HelpRequestComponent implements OnInit {
   helpRequest: string;
-  accountEncrypt: string;
-  drawPublicButton: string;
-
-  private routeSubscription: Subscription;
 
   constructor(
     private dataService: DataService,
     private notifier: NotifierService
-  ) {
-    this.accountEncrypt = localStorage.getItem('accountEncrypt');
-  }
+  ) { }
 
   ngOnInit() {
   }
 
   sendToSupport() {
-    this.accountEncrypt = localStorage.getItem('accountEncrypt');
-
     if (!this.helpRequest) {
       this.notifier.warning('Напишите ваш вопрос в поле для ввода текста', 'Пустое сообщение!');
-      return;
-    }
-
-    if (!this.accountEncrypt) {
-      this.notifier.warning('Только зарегистрированные пользователи могут отправлять запросы в службу поддержки', 'Зарегистрируйтесь!');
       return;
     }
 
