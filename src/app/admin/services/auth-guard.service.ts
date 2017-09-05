f0bbdf5d2532c6e9b13465750ebae751d3e3ea0f
snizh <snizh@list.ru>
2017-09-05 12:09:36 +1000

    Заменил заглушку в сервисе AuthGuard на реальную проверку

--- src/app/admin/services/auth-guard.service.ts
+++ src/app/admin/services/auth-guard.service.ts
@@ -2,22 +2,25 @@ import { Injectable } from '@angular/core';
 import {
   Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot
 } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+  ) { }
 
   canActivate(
     activatedRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    // if (localStorage.getItem('accountEncrypt')) {
-    //   // logged in so return true
-    //   return true;
-    // }
-    // this.router.navigate(['/admin/login']);
-    // return false;
-    return true;
+    if (this.auth.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/admin/login']);
+      return false;
+    }
   }
 }
