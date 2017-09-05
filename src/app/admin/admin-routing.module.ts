af04ddfe101d601c11e64b59c057a86e18d81436
snizh <snizh@list.ru>
2017-09-05 12:48:23 +1000

    Переделал маршрутизацию на более частую проверку AuthGuard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AdminComponent} from './admin.component';
-import { LoginComponent} from './login/login.component';
+import { AdminComponent } from './admin.component';
+import { LoginComponent } from './login/login.component';
 import { HelpRequestComponent } from './help/help-request.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ParticipantsListComponent } from './participants/participants-list.component';
@@ -13,24 +13,18 @@ import { DocumentationComponent } from './documentation/documentation.component'
 import { AuthGuard } from './services/auth-guard.service';
 
 const routes: Routes = [{
-    path: '',
-    component: AdminComponent,
-    children: [
-      { path: 'documentation', component: DocumentationComponent },
-      { path: 'login', component: LoginComponent },
-      { path: '',
-        canActivate: [AuthGuard],
-        children: [
-          { path: 'dashboard', component: DashboardComponent  },
-          { path: 'participants-list', component: ParticipantsListComponent },
-          { path: 'circular', component: CircularComponent },
-          { path: 'bonus-accounting', component: ParticipantsListComponent },
-          { path: 'settings', component: ReferralSettingsComponent },
-          { path: 'help', component: HelpRequestComponent },
-        ]
-      }
-//      { path: '**', component: LoginComponent }
-    ]
+  path: '',
+  component: AdminComponent,
+  children: [
+    { path: 'documentation', component: DocumentationComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+    { path: 'participants-list', component: ParticipantsListComponent, canActivate: [AuthGuard] },
+    { path: 'circular', component: CircularComponent, canActivate: [AuthGuard] },
+    { path: 'bonus-accounting', component: ParticipantsListComponent, canActivate: [AuthGuard] },
+    { path: 'settings', component: ReferralSettingsComponent, canActivate: [AuthGuard] },
+    { path: 'help', component: HelpRequestComponent, canActivate: [AuthGuard] },
+  ]
 }];
 
 @NgModule({
