339ad5cbdd728d3bc8f1ead47e7f2083d3191f3b
Sergei Nizhivenko <snizh@list.ru>
2017-10-30 12:15:42 +1000

    Закомментировал неиспользуемые в приложении user функции.

--- src-user/app/user/services/utils.service.ts
+++ src-user/app/user/services/utils.service.ts
@@ -10,16 +10,16 @@ export class UtilsService {
   //   return n ? n.toFixed(size) : zero;
   // }
 
-  formatDate(d: Date) {
-    return Intl.DateTimeFormat('ru-RU').format(d);
-  }
+  // formatDate(d: Date) {
+  //   return Intl.DateTimeFormat('ru-RU').format(d);
+  // }
 
-  parseDate(s: string) {
-    const ps = s
-      .split('.')
-      .map(v => parseInt(v, 10));
-    return new Date(ps[2], ps[1] - 1, ps[0]);
-  }
+  // parseDate(s: string) {
+  //   const ps = s
+  //     .split('.')
+  //     .map(v => parseInt(v, 10));
+  //   return new Date(ps[2], ps[1] - 1, ps[0]);
+  // }
 
   parseNumber(v: string|number) {
     return isNaN(parseFloat(String(v))) ? 0 : parseFloat(String(v));
