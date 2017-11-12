eae6213785427428fd7a913ae86f7462932110a7
Sergei Nizhivenko <snizh@list.ru>
2017-11-13 00:35:44 +1000

    Убрал из заголовка отчета надпись under construction!

--- src/app/admin/reports/abc-analysis/abc-analysis-report.component.ts
+++ src/app/admin/reports/abc-analysis/abc-analysis-report.component.ts
@@ -25,7 +25,6 @@ export class ABCAnalysisReportComponent implements OnInit {
   startDate: Date;
   endDate: Date;
   reportTableData: TreeNode[] = [];
-  reportHeader = 'ABC-анализ';
   loading: boolean;
 
   constructor(
