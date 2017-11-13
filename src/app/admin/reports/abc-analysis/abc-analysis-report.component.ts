6d5db48367763030bd4266d8bd4b2fb691211c50
Sergei Nizhivenko <snizh@list.ru>
2017-11-13 13:01:07 +1000

    Добавил немного проверок типов

--- src/app/admin/reports/abc-analysis/abc-analysis-report.component.ts
+++ src/app/admin/reports/abc-analysis/abc-analysis-report.component.ts
@@ -23,37 +23,38 @@ interface TableRow {
 export class ABCAnalysisReportComponent implements OnInit {
   readonly calendarLocale = russianCalendarLocale;
   maxRowsPerPage = 14;
-  reportData = new ReportModel;
+  reportData: ReportModel;
   loading: boolean;
 
   constructor(
     private router: Router,
     private abcS: ABCAnalysisService,
   ) {
+    this.reportData = new ReportModel();
     this.reportData.table.body = [];
     this.reportData.endDate = new Date();
     this.reportData.beginDate = localBeginningOfTheYear(this.reportData.endDate);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getMainReport();
   }
 
-  getMainReport() {
+  getMainReport(): void {
     this.loading = true;
     this.reportData.table.body = [];
     this.abcS.getReportData(this.reportData.beginDate, this.reportData.endDate)
       .subscribe( repData => {
-        this.reportData = repData;
+        this.reportData = <ReportModel>repData;
         this.loading = false;
       });
   }
 
-  getDetails(selectedRow: TableRow) {
+  getDetails(selectedRow: TableRow): void {
     this.router.navigate(['/admin/participants/' + encodeURIComponent(selectedRow.id)]);
   }
 
-  isNumber(v: any) {
+  isNumber(v: any): boolean {
     return typeof v === 'number';
   }
 }
