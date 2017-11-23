722b15946af7765f2caed0808363377cf1065375
Sergei Nizhivenko <snizh@list.ru>
2017-11-23 17:32:26 +1000

    Добавил в детализированный отчет извлечение параметров из url

--- src/app/admin/reports/participant-satisfaction/participant-satisfaction-detailed-report.component.ts
+++ src/app/admin/reports/participant-satisfaction/participant-satisfaction-detailed-report.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
+import { localBeginningOfTheYear } from '../../../shared/utils';
 import { ParticipantSatisfactionService } from './participant-satisfaction.service';
 import { ReportModel } from '../shared/report.model';
 
@@ -13,21 +15,39 @@ export class ParticipantSatisfactionDetailedReportComponent implements OnInit {
   reportData: ReportModel;
   loading: boolean;
 
+  participantId: string;
+
   constructor(
     private location: Location,
+    private route: ActivatedRoute,
     private dataService: ParticipantSatisfactionService,
   ) {
     this.reportData = new ReportModel();
   }
 
+  // если URL-параметр парсится как дата, то возвращает Date, иначе - null
+  private parseDateFromParam(dateParam: string): Date | null {
+    return isNaN(Date.parse(decodeURIComponent(dateParam))) ?
+      null :
+      new Date(decodeURIComponent(dateParam));
+  }
+
   ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+
+    this.participantId = routeParams.get('id') || '';
+    this.reportData.endDate = this.parseDateFromParam(routeParams.get('enddate'))
+      || new Date();
+    this.reportData.beginDate = this.parseDateFromParam(routeParams.get('begindate'))
+      || localBeginningOfTheYear(this.reportData.endDate);
+
     this.getMainReport();
   }
 
   getMainReport(): void {
     this.loading = true;
     this.reportData.table.body = [];
-    this.dataService.getDetailedReportData(this.reportData.beginDate, this.reportData.endDate)
+    this.dataService.getDetailedReportData(this.participantId, this.reportData.beginDate, this.reportData.endDate)
       .subscribe( repData => {
         this.reportData = <ReportModel>repData;
         this.loading = false;
